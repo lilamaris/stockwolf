9abe4e1a3f000e79669fd77d284d204f8d617eaf
lilamaris <lilamaris.dev@gmail.com>
2026-01-26 11:57:44 +0900

    initial

---
+++ src/main/java/com/lilamaris/stockwolf/StockwolfApplication.java
@@ -0,0 +1,13 @@
+package com.lilamaris.stockwolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockwolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockwolfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilamaris/stockwolf/StockwolfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilamaris.stockwolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockwolfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
