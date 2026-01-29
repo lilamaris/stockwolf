f781605bec4b8e48639e44351ed60cd7b4877191
lilamaris <lilamaris.dev@gmail.com>
2026-01-30 00:33:55 +0900

    fix(inventory): resilient 활성화 어노테이션 누락 수정
    - 이거 없으면 @Retryable 어노테이션 무시됨. 이전 커밋에서 누락..

--- inventory/src/main/java/com/lilamaris/stockwolf/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/lilamaris/stockwolf/inventory/InventoryApplication.java
@@ -3,8 +3,10 @@ package com.lilamaris.stockwolf.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @EnableJpaAuditing
+@EnableResilientMethods
 @SpringBootApplication
 public class InventoryApplication {
     public static void main(String[] args) {
