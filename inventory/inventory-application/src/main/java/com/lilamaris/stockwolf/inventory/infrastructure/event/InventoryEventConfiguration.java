b805614dac39c1ffdd7dc259c8f3fb89e697e2a1
lilamaris <lilamaris.dev@gmail.com>
2026-03-17 13:06:06 +0900

    feat(inventory): Order 이벤트 리스너 정의

--- inventory/inventory-application/src/main/java/com/lilamaris/stockwolf/inventory/infrastructure/event/InventoryEventConfiguration.java
+++ inventory/inventory-application/src/main/java/com/lilamaris/stockwolf/inventory/infrastructure/event/InventoryEventConfiguration.java
@@ -1,13 +1,19 @@
 package com.lilamaris.stockwolf.inventory.infrastructure.event;
 
-import com.lilamaris.stockwolf.inventory.infrastructure.event.listener.OrderCreatedListener;
+import com.lilamaris.stockwolf.inventory.infrastructure.event.listener.OrderCanceledEventListener;
+import com.lilamaris.stockwolf.inventory.infrastructure.event.listener.OrderCreatedEventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InventoryEventConfiguration {
     @Bean
-    OrderCreatedListener orderCreatedListener() {
-        return new OrderCreatedListener();
+    OrderCreatedEventListener orderCreatedListener() {
+        return new OrderCreatedEventListener();
+    }
+
+    @Bean
+    OrderCanceledEventListener orderCanceledEventListener() {
+        return new OrderCanceledEventListener();
     }
 }
--- inventory/inventory-application/src/main/java/com/lilamaris/stockwolf/inventory/infrastructure/event/listener/OrderCreatedEventListener.java
+++ inventory/inventory-application/src/main/java/com/lilamaris/stockwolf/inventory/infrastructure/event/listener/OrderCreatedEventListener.java
@@ -5,24 +5,24 @@ import com.lilamaris.stockwolf.event.core.EventKey;
 import com.lilamaris.stockwolf.event.core.EventTrace;
 import com.lilamaris.stockwolf.event.core.inbound.EventListener;
 import com.lilamaris.stockwolf.order.contract.event.definition.OrderEventKey;
-import com.lilamaris.stockwolf.order.contract.event.payload.OrderCanceledEventPayload;
+import com.lilamaris.stockwolf.order.contract.event.payload.OrderCreatedEventPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
-public class OrderCanceledEventListener implements EventListener<OrderCanceledEventPayload> {
+public class OrderCreatedEventListener implements EventListener<OrderCreatedEventPayload> {
     @Override
     public EventKey key() {
-        return OrderEventKey.ORDER_CANCELED;
+        return OrderEventKey.ORDER_CREATED;
     }
 
     @Override
-    public Class<OrderCanceledEventPayload> payload() {
-        return OrderCanceledEventPayload.class;
+    public Class<OrderCreatedEventPayload> payload() {
+        return OrderCreatedEventPayload.class;
     }
 
-    public void handle(EventHeader header, EventTrace trace, OrderCanceledEventPayload payload) {
+    public void handle(EventHeader header, EventTrace trace, OrderCreatedEventPayload payload) {
         log.info("this is {}, and i got message from {}, key is {}", "inventory-service", trace.producer(), header.eventKey());
     }
 }
