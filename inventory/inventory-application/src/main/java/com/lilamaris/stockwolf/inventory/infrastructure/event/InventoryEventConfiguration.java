7255f892b9dff604b17c68602ab8c176c15314eb
lilamaris <lilamaris.dev@gmail.com>
2026-03-17 13:24:22 +0900

    feat(inventory.event): 이벤트 라우터 변경
    - 기본 코어 제공 LogOnly 이벤트 라우터 대신 Listener Registry 기반 이벤트 라우터 사용

--- inventory/inventory-application/src/main/java/com/lilamaris/stockwolf/inventory/infrastructure/event/InventoryEventConfiguration.java
+++ inventory/inventory-application/src/main/java/com/lilamaris/stockwolf/inventory/infrastructure/event/InventoryEventConfiguration.java
@@ -1,5 +1,9 @@
 package com.lilamaris.stockwolf.inventory.infrastructure.event;
 
+import com.lilamaris.stockwolf.event.core.inbound.EventListenerRegistrar;
+import com.lilamaris.stockwolf.event.core.inbound.EventRouter;
+import com.lilamaris.stockwolf.event.core.inbound.SimpleEventRouter;
+import com.lilamaris.stockwolf.event.core.serializer.EventDeserializer;
 import com.lilamaris.stockwolf.inventory.infrastructure.event.listener.OrderCanceledEventListener;
 import com.lilamaris.stockwolf.inventory.infrastructure.event.listener.OrderCreatedEventListener;
 import org.springframework.context.annotation.Bean;
@@ -15,5 +19,16 @@ public class InventoryEventConfiguration {
     @Bean
     OrderCanceledEventListener orderCanceledEventListener() {
         return new OrderCanceledEventListener();
+    }
+
+    @Bean
+    EventRouter eventRouter(
+            EventListenerRegistrar eventListenerRegistrar,
+            EventDeserializer eventDeserializer
+    ) {
+        return new SimpleEventRouter(
+                eventListenerRegistrar,
+                eventDeserializer
+        );
     }
 }
